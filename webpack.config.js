f688437f5933cee2fbdd75756ae24e3d78e2a182
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2021-03-24 15:01:41 -0500

    got rid of css requirements

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 // const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 // const deps = require('./package.json').dependencies
@@ -20,13 +19,6 @@ module.exports = {
 
   module: {
     rules: [
-      // {
-      //   test: /\.m?js/,
-      //   type: 'javascript/auto',
-      //   resolve: {
-      //     fullySpecified: false,
-      //   },
-      // },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
@@ -34,17 +26,6 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
-      // {
-      //   test: /\.css$/i,
-      //   use: [
-      //     {
-      //       loader: MiniCssExtractPlugin.loader,
-      //       // options for importing images in css
-      //       options: { publicPath: '' },
-      //     },
-      //     'css-loader',
-      //   ],
-      // },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         /* alt: asset/resources or asset/inline */
@@ -55,7 +36,6 @@ module.exports = {
 
   plugins: [
     new CleanWebpackPlugin(),
-    // new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
@@ -81,8 +61,6 @@ module.exports = {
     //   },
     // }),
   ],
-
-  devtool: 'source-map',
 
   devServer: {
     port: 8000,
